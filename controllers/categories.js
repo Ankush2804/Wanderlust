bd227b18ef133145c07fe8778277d259fa1f566f
Ankush Kumar <ankushop111@gmail.com>
2025-10-03 01:26:57 +0530

    Add Project Files

---
+++ controllers/categories.js
@@ -0,0 +1,14 @@
+
+const categories = [
+  "Beach",
+  "Rooms",
+  "Iconic cities",
+  "Mountains",
+  "Castles",
+  "Amazing pools",
+  "Camping",
+  "Farms",
+  "Arctic"
+];
+
+module.exports = categories;
